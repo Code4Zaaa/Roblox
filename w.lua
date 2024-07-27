6f92f3195428a446ba1e6488193949bc3d0537bb
Code4Zaaa <98988528+Code4Zaaa@users.noreply.github.com>
2024-07-27 16:53:42 +0700

    Update w.lua

--- w.lua
+++ w.lua
@@ -1,5 +1,5 @@
 return (function(...)
-    local args = ...  -- Unpack the arguments into a table
+    local args = ...
     -- GHUB {13772394625, 3579144, 9318753}
     if (args[1] == 13772394625 and args[2] == 3579144 and args[3] == 9318753) then
         print("Ganteng Hub")
