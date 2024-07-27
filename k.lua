1cbf6075c5efbee687c115fada92ab69673cf8d5
Code4Zaaa <98988528+Code4Zaaa@users.noreply.github.com>
2024-07-27 16:38:07 +0700

    Delete k.lua

--- k.lua
+++
@@ -1,8 +0,0 @@
-print(...)
-return function(...)
-    print("Received parameters:")
-    for i, v in ipairs(...) do
-        print(i, v)
-    end
-    -- Your custom logic here using the passed parameters
-end)
