34adfefa3d9176330d59b769469be08640ff6d17
Code4Zaaa <98988528+Code4Zaaa@users.noreply.github.com>
2024-07-27 16:33:33 +0700

    Update k.lua

--- k.lua
+++ k.lua
@@ -1,9 +1,8 @@
 print("tes")
-print(...)
-return function(...)
+return (function(...)
     print("Received parameters:")
     for i, v in ipairs(...) do
         print(i, v)
     end
     -- Your custom logic here using the passed parameters
-end
+end)()
