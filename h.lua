894993db97bfc6cdc878f60c94688339e1c16186
Code4Zaaa <98988528+Code4Zaaa@users.noreply.github.com>
2024-07-27 16:50:36 +0700

    Update h.lua

--- h.lua
+++ h.lua
@@ -2,7 +2,7 @@ return (function(...)
     local args = {...}  -- Unpack the arguments into a table
     -- GHUB {13772394625, 3579144, 9318753}
     table.foreach(args, print)
-    if (args[1] == 13772394625 and args[2] == 3579144 and args[3] == 9318753) then
+    if (args[1][1] == 13772394625 and args[1][2] == 3579144 and args[1][3] == 9318753) then
         print("Ganteng Hub")
     end
 end)(...)
